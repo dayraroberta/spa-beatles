64fcc70159cf25453c0d05c0fae595d0d134cb80
dayraroberta <dayraroberta@icloud.com>
2018-10-31 17:08:42 -0200

    Agora vai (finalizado)

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 
 const Header = () => (
   <Row>  
-    <Navbar className="indigo lighten-2">
+    <Navbar className="yellow accent-4">
       <li><NavLink to="/">Home</NavLink></li>
       <li><NavLink to="john-lennon">John Lennon</NavLink></li>
       <li><NavLink to="paul-mccartney">Paul McCartney</NavLink></li>
